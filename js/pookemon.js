fcc6ad294687d5f51d8a7d203e429bb83426ed2d
Cecilia Cerqueda <pzcerqueda@gmail.com>
2017-03-29 23:44:10 -0600

    pelea de pokemones

---
+++ js/pookemon.js
@@ -0,0 +1,34 @@
+function Pokemon(nombre,color,puntosAtaque){
+  this.nombre = nombre;
+  this.color = color;
+  this.nivelDeAmistad = 0;
+  this.vida = 100;
+  this.puntosAtaque = puntosAtaque;
+
+  this.saludar = function(){
+    console.log("Hola, me llamo " + this.nombre)
+  };
+
+  this.pelear = function(pokemonObjeto){
+    pokemonObjeto.vida = pokemonObjeto.vida - this.puntosAtaque
+  }
+
+  this.beber = function(tipoAlcohol){
+    if(tipoAlcohol == "chelas"){
+      this.nivelDeAmistad += 80
+    }
+    else if (tipoAlcohol == "vodka") {
+      this.nivelDeAmistad += 10
+    }
+  };
+}
+
+var pikachu = new Pokemon("Pikachu","amarillo",80);
+var charmander  = new Pokemon("Charmander","rojo",30);
+
+console.log(pikachu)
+console.log(charmander)
+
+pikachu.pelear(charmander)
+
+console.log(charmander.vida)
